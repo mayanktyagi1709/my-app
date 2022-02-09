4350b6d1a71a90e6cdd218abd2eda76ea388e12e
mayanktyagi1709 <mayanktyagi1709@gmail.com>
2022-02-09 15:19:52 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import File from "./components/File";
 
-function App() {
+const App = () => {
+  // const client_id = process.env.REACT_APP_ID;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <File per_page={20} />
     </div>
   );
-}
+};
 
 export default App;
